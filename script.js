c6611b7a603660efe30686bc5bc3551d4d4014b1
Kobe Lean <kobeazetro@gmail.com>
2026-05-16 23:49:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 
+const btn = document.getElementById('loginBtn')
+const loading = document.getElementById('loading')
+
+btn.addEventListener('click', () => {
+
+  btn.style.display = 'none'
+  loading.classList.remove('hidden')
+
+  const steps = [
+    'Checking profile...',
+    'Analyzing behavior...',
+    'Detecting obsession level...',
+    'Finalizing report...'
+  ]
+
+  let i = 0
+
+  const text = loading.querySelector('p')
+
+  const interval = setInterval(() => {
+
+    if (i < steps.length) {
+      text.innerText = steps[i]
+      i++
+    }
+
+  }, 900)
+
+  setTimeout(() => {
+    clearInterval(interval)
+    window.location.href = 'caught.html'
+  }, 4200)
+
+})
